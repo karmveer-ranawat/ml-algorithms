f0cffaec61a8a842332069560d6d5c4d40e7b8de
Cyber Wizard <karmveerranawat+github@gmail.com>
2025-04-08 22:24:26 +0530

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,10 @@
+import numpy as np
+# creating first matrix
+A = np.array([[1, 2], [3, 4]])
+# creating second matrix
+B = np.array([[4, 5], [6, 7]])
+print("Printing elements of first matrix")print(A)
+print("Printing elements of second matrix")
+print(B)
+# adding two matrix pri
+print(np.add(A, B))
